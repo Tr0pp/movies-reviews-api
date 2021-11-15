f42b4651bcadab4c7736e9f7d84bc53b75c8a56a
Tr0pp <rodrigooh.dl@gmail.com>
2021-11-15 14:16:51 -0300

    ajuste navbar + build

--- web/src/components/shared/Navbar.vue
+++ web/src/components/shared/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
     <b-navbar toggleable="lg" type="dark" variant="dark">
-      <b-navbar-brand href="/home" class="nv">Movies Reviews</b-navbar-brand>
+      <b-navbar-brand href="/" class="nv">Movies Reviews</b-navbar-brand>
 
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
